b1ce373a99dedc8e8007b7077c3f83161254dce7
Emily Reynolds <fireandicefangirl@gmail.com>
2024-04-10 13:47:43 -0600

    Wired up home Controller

--- src/main/java/com/petpedia/web/home/HomeController.java
+++ src/main/java/com/petpedia/web/home/HomeController.java
@@ -1,9 +1,21 @@
 package com.petpedia.web.home;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @RequiredArgsConstructor
 public class HomeController {
+
+    @GetMapping("/")
+    public String welcome(@AuthenticationPrincipal UserDetails userDetails, Model model){
+        var authenticated = userDetails != null;
+        model.addAttribute("currentRoute", "home");
+        model.addAttribute("isAuthenticated", authenticated);
+        return "home";
+    }
 }
