22728e7f2b444d826e6d8094002fe57d38759807
Emily Reynolds <fireandicefangirl@gmail.com>
2024-04-11 20:07:30 -0600

    Added Adison's changes into home.html and started making the home page look better

--- src/main/java/com/petpedia/web/home/HomeController.java
+++ src/main/java/com/petpedia/web/home/HomeController.java
@@ -2,8 +2,15 @@ package com.petpedia.web.home;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
 public class HomeController {
+
+    @RequestMapping("/")
+    public String home() {
+        return "home";
+    }
+
 }
