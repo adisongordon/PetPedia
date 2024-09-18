21bf46077d36ae938097b6b812bf894b56c537d2
Emily Reynolds <fireandicefangirl@gmail.com>
2024-09-17 18:45:19 -0600

    added content for AccountController

--- src/main/java/com/petpedia/web/controllers/AccountController.java
+++ src/main/java/com/petpedia/web/controllers/AccountController.java
@@ -1,4 +1,27 @@
 package com.petpedia.web.controllers;
 
+import com.petpedia.web.model.Users;
+import com.petpedia.web.model.UsersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
 public class AccountController {
+
+    @Autowired
+    private UsersRepository usersRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @PostMapping(value = "/req/signup", consumes = "application/json")
+    public Users createUser(@RequestBody Users user){
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return usersRepository.save(user);
+    }
+
 }
