f76a57be6493d322a2419bcd24c0fa729aa711b9
Emily Reynolds <fireandicefangirl@gmail.com>
2024-09-17 18:34:40 -0600

    remove old directories, add new directories

--- src/main/java/com/petpedia/web/faqs/FrequentlyAskedQuestionsController.java
+++
@@ -1,9 +0,0 @@
-package com.petpedia.web.faqs;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
-
-@Controller
-@RequiredArgsConstructor
-public class FrequentlyAskedQuestionsController {
-}
