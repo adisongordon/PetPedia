8860181917dc556936da26a300f10068eba42d0f
Emily Reynolds <fireandicefangirl@gmail.com>
2024-09-17 18:50:49 -0600

    added necessary stuff to PetPediaApplication

--- src/main/java/com/petpedia/web/PetPediaApplication.java
+++ src/main/java/com/petpedia/web/PetPediaApplication.java
@@ -1,13 +1,27 @@
 package com.petpedia.web;
 
+import com.petpedia.web.model.WikiDataRepository;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class PetPediaApplication {
 
+    @Autowired
+    private WikiDataRepository wikiDataRepository;
+
     public static void main(String[] args) {
         SpringApplication.run(PetPediaApplication.class, args);
+    }
+
+    @Bean
+    InitializingBean populateDatabase() {
+        return () -> {
+            // TODO Populate wiki data here
+        };
     }
 
 }
